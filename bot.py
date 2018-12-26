28a7da87d937c657932a6dd5c4f812e61b58384f
Huskie <38168951+SibHuskie@users.noreply.github.com>
2018-12-26 16:25:59 +1100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import random
+import pickle
+import os
+import os.path
+import requests
+import json
+import time
+from gtts import gTTS
+import urbandictionary as ud
+
+##################################
+client.run(os.environ['BOT_TOKEN'])
