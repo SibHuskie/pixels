ee9f2f6205504fb56fffbabe97bef62700a94e3a
Huskie <38168951+SibHuskie@users.noreply.github.com>
2018-12-26 17:45:39 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ client = commands.Bot(command_prefix=bot_prefix)
 server = discord.Server(id='439658556656975872')
 footer_text = "Pixels™"
 error_img = ':x: '
+limit = 100000000000000000
 default_invite = 'https://discord.gg/Xj6beq7'
 banner = 'https://cdn.discordapp.com/attachments/484761617016291328/527361105006297108/Photoshop_ccpixel.png'
 
