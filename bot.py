15e5922c3d31c72b003f6d1f7b460e88c33c185e
Huskie <38168951+SibHuskie@users.noreply.github.com>
2018-12-26 17:04:15 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,5 +21,29 @@ error_img = ':x: '
 default_invite = 'https://discord.gg/Xj6beq7'
 banner = 'https://cdn.discordapp.com/attachments/484761617016291328/527361105006297108/Photoshop_ccpixel.png'
 
+# say <text>
+@client.command(pass_context=True)
+async def say(ctx, *, args = None):
+    author = ctx.message.author
+    vip = discord.utils.get(ctx.message.server.roles, id='Moderator')
+    legend = discord.utils.get(ctx.message.server.roles, name='<@299761993382887425>')
+    msg = discord.Embed(colour=0x284e87, description= "")
+    msg.title = ""
+    msg.set_footer(text=footer_text)
+    if vip in author.roles or legend in author.roles:
+        if args == None:
+            msg.add_field(name=error_img, value="Please give a message that you want the bot to say.")
+            await client.say(embed=msg)
+        else:
+            if len(str(args)) > 2000:
+                msg.add_field(name=error_img, value="The message cannot be longer than 2000 characters.")
+                await client.say(embed=msg)
+            else:
+                await client.say("{}".format(args))
+                await client.delete_message(ctx.message)
+    else:
+        msg.add_field(name=error_img, value="This command can only be used by Admins!")
+        await client.say(embed=msg)
+        
 ##################################
 client.run(os.environ['BOT_TOKEN'])
