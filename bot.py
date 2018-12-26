c1b0c7103ba50ee87eda1b22a7fcdf77558fea9b
Huskie <38168951+SibHuskie@users.noreply.github.com>
2018-12-26 16:45:19 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,5 +12,14 @@ import time
 from gtts import gTTS
 import urbandictionary as ud
 
+Client = discord.Client()
+bot_prefix= "Px!"
+client = commands.Bot(command_prefix=bot_prefix)
+server = discord.Server(id='439658556656975872')
+footer_text = "Pixels™"
+error_img = ':x: '
+default_invite = 'https://discord.gg/Xj6beq7'
+banner = 'https://cdn.discordapp.com/attachments/484761617016291328/527361105006297108/Photoshop_ccpixel.png'
+
 ##################################
 client.run(os.environ['BOT_TOKEN'])
